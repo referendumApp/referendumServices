bf7c87f418c83c76c9387d6a7df125cacf9801fe
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-09-13 10:41:36 -0700

    initial app

---
+++ api/app.py
@@ -0,0 +1,51 @@
+import json
+import boto3
+import logging
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+s3 = boto3.client('s3')
+
+BUCKET_NAME = 'referendum-alpha/feedback'
+FILE_NAME = 'feedback.json'
+
+
+@app.post("/feedback")
+async def add_feedback(feedback: dict):
+    try:
+        try:
+            response = s3.get_object(Bucket=BUCKET_NAME, Key=FILE_NAME)
+            file_content = json.loads(response['Body'].read().decode('utf-8'))
+        except s3.exceptions.NoSuchKey:
+            logger.warning(f"File {FILE_NAME} not found in bucket {BUCKET_NAME}. Creating new file.")
+            file_content = {"feedbackMessages": []}
+
+        file_content['feedbackMessages'].append(feedback)
+
+        s3.put_object(
+            Bucket=BUCKET_NAME,
+            Key=FILE_NAME,
+            Body=json.dumps(file_content),
+            ContentType='application/json'
+        )
+        return
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
