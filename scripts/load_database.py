f170c406fe9fec3b949a7af063fbd0e4c05e14fa
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-09-29 20:19:16 -0700

    run black

--- scripts/load_database.py
+++ scripts/load_database.py
@@ -5,24 +5,30 @@ from sqlalchemy import create_engine
 from sqlalchemy.types import JSON
 
 # Database connection
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 
 def load_json_files(directory):
     for filename in os.listdir(directory):
-        if filename.endswith('.json'):
+        if filename.endswith(".json"):
             file_path = os.path.join(directory, filename)
             try:
-                with open(file_path, 'r') as file:
+                with open(file_path, "r") as file:
                     data = json.load(file)
 
                 df = pd.json_normalize(data)
 
-                table_name = filename.replace('.json', '')
+                table_name = filename.replace(".json", "")
 
                 # Write to database
-                df.to_sql(table_name, engine, if_exists='replace', index=False, dtype={'data': JSON})
+                df.to_sql(
+                    table_name,
+                    engine,
+                    if_exists="replace",
+                    index=False,
+                    dtype={"data": JSON},
+                )
 
                 print(f"Loaded data from {filename} into table {table_name}")
             except Exception as e:
