8b4b4a42294fed7f7e7f1079d6795283cc10a362
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-10-02 13:25:06 -0700

    moving things around

--- common/database/legiscan/connection.py
+++ common/database/legiscan/connection.py
@@ -1,5 +1,5 @@
 import os
 
-from database.common.core import create_session
+from common.database.postgres_core.utils import create_session
 
 SessionLocal = create_session(db_name=os.getenv("LEGISCAN_API_DB_NAME"))
--- common/database/referendum/connection.py
+++ common/database/referendum/connection.py
@@ -1,5 +1,5 @@
 import os
 
-from database.common.core import create_session
+from common.database.postgres_core.utils import create_session
 
 SessionLocal = create_session(db_name=os.getenv("REFERENDUM_DB_NAME"))
