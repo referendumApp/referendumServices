7c4ea04c084e550f5fb5554f249c61cb610a2005
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-10-02 15:50:09 -0700

    make token inaccessible

--- api/app.py
+++ api/app.py
@@ -64,17 +64,17 @@ async def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user, hashed_password=hashed_password)
 
 
-@app.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token = create_access_token(data={"sub": user.email})
-    return {"access_token": access_token, "token_type": "bearer"}
+# @app.post("/token", response_model=Token)
+# async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+#     user = authenticate_user(db, form_data.username, form_data.password)
+#     if not user:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Incorrect username or password",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+#     access_token = create_access_token(data={"sub": user.email})
+#     return {"access_token": access_token, "token_type": "bearer"}
 
 
 ########################################################################################################
