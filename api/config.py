3e135cda33497dc5c821d88c116c458643d22aa3
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-10-02 15:16:24 -0700

    use config

--- api/config.py
+++ api/config.py
@@ -3,6 +3,12 @@ from functools import lru_cache
 
 
 class Settings(BaseSettings):
+    # API
+    PROJECT_NAME: str = "Referendum App"
+    LOG_LEVEL: str = "INFO"
+
+    # AWS
+    AWS_REGION: str | None = "us-east-2"
     ALPHA_BUCKET_NAME: str = "referendum-app-alpha"
     FEEDBACK_FILE_NAME: str = "feedback.json"
 
