6fb5251806f3f2be1d2b4bf307f2439484112551
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-10-10 16:43:24 -0700

    fix fucked up merge

--- common/database/referendum/schemas.py
+++ common/database/referendum/schemas.py
@@ -113,6 +113,7 @@ class Topic(TopicBase):
 
 
 class LegislatorBase(BaseModel):
+    legiscan_id: int
     name: str
     image_url: Optional[str]
     district: str
@@ -172,39 +173,6 @@ class Bill(BillRecord):
     legislative_body: Optional[LegislativeBody] = None
     topics: List[Topic] = []
     sponsors: List[LegislatorRecord] = []
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-# Legislators
-
-
-class LegislatorBase(BaseModel):
-    legiscan_id: int
-    name: str
-    image_url: Optional[str]
-    district: str
-    party_id: int
-
-    address: Optional[str] = None
-    facebook: Optional[str] = None
-    instagram: Optional[str] = None
-    phone: Optional[str] = None
-    twitter: Optional[str] = None
-
-
-class LegislatorCreate(LegislatorBase):
-    pass
-
-
-class LegislatorRecord(LegislatorBase):
-    id: int
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class Legislator(LegislatorRecord):
-    committees: List[Committee]
 
     model_config = ConfigDict(from_attributes=True)
 
