c1fb1a4daefa8ed5754f09820c6da4e7126e5db6
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-10-25 08:31:25 -0700

    logging and kickoff

--- common/database/postgres_core/utils.py
+++ common/database/postgres_core/utils.py
@@ -1,24 +1,26 @@
 import os
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def get_connection_string(db_name: str):
     if not db_name:
-        raise ValueError(
-            f"Missing required database configuration: {', '.join(missing)}"
-        )
+        raise ValueError("Missing db_name")
 
     user = os.getenv("POSTGRES_USER")
     password = os.getenv("POSTGRES_PASSWORD")
     host = os.getenv("POSTGRES_HOST")
     port = os.getenv("POSTGRES_PORT")
 
-    print(f"Attempting database connection with:")
-    print(f"User: {user}")
-    print(f"Host: {host}")
-    print(f"Port: {port}")
-    print(f"DB Name: {db_name}")
+    logger.info(f"Attempting database connection with:")
+    logger.info(f"User: {user}")
+    logger.info(f"Host: {host}")
+    logger.info(f"Port: {port}")
+    logger.info(f"DB Name: {db_name}")
 
     if not user:
         raise ValueError(f"POSTGRES_USER environment variable is not set")
--- pipeline/run.py
+++ pipeline/run.py
@@ -38,7 +38,6 @@ def check_db_connection(db_session):
 
 
 def extract(etl_configs) -> Dict[str, pd.DataFrame]:
-    logger.info("EXTRACT: Extracting data")
     legiscan_db = next(get_legiscan_api_db())
 
     if check_db_connection(legiscan_db):
@@ -46,6 +45,7 @@ def extract(etl_configs) -> Dict[str, pd.DataFrame]:
         with legiscan_db.connection() as conn:
             for config in etl_configs:
                 table_name = config["source"]
+                logger.info(f"Extracting table {table_name}")
 
                 try:
                     df = pd.read_sql(table_name, con=conn)
@@ -68,9 +68,9 @@ def extract(etl_configs) -> Dict[str, pd.DataFrame]:
 
 
 def transform(etl_configs) -> Dict[str, pd.DataFrame]:
-    logger.info("EXTRACT: Transforming data")
     for config in etl_configs:
         table_name = config["source"]
+        logger.info(f"Transforming table {table_name}")
 
         try:
 
@@ -105,7 +105,6 @@ def transform(etl_configs) -> Dict[str, pd.DataFrame]:
 
 
 def load(etl_configs):
-    logger.info("EXTRACT: Loading data")
     referendum_db = next(get_referendum_db())
 
     if check_db_connection(referendum_db):
@@ -125,6 +124,7 @@ def load(etl_configs):
         # Process the ETL load
         for config in etl_configs:
             destination_table = config["destination"]
+            logger.info(f"Loading table {destination_table}")
             df = config["dataframe"]
 
             try:
@@ -353,14 +353,18 @@ def orchestrate_etl():
         # },
     ]
     try:
+        logger.info("ETL process starting")
+        logger.info("Beginning extraction")
         etl_configs = extract(etl_configs)
+        logger.info("Beginning transformation")
         etl_configs = transform(etl_configs)
+        logger.info("Beginning load")
         load(etl_configs)
         logger.info("ETL process completed successfully")
     except ConnectionError as e:
         logger.error(f"ETL process failed: {str(e)}")
     except Exception as e:
-        logger.error(f"An unexpected error occurred during ETL process: {str(e)}")
+        logger.error(f"ETL process failed with unexpected error: {str(e)}")
 
 
 if __name__ == "__main__":
