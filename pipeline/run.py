85161717b0419dc93634d77636c428546ae0567e
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-10-29 13:39:02 -0700

    simplify

--- pipeline/run.py
+++ pipeline/run.py
@@ -17,6 +17,9 @@ logger = logging.getLogger(__name__)
 
 def get_legiscan_api_db():
     db = legiscan_api_connection.SessionLocal()
+    if not check_db_connection(db):
+        logger.error("Failed to connect to legiscan_api database")
+        raise ConnectionError("legiscan_api database connection failed")
     try:
         yield db
     finally:
@@ -25,6 +28,9 @@ def get_legiscan_api_db():
 
 def get_referendum_db():
     db = referendum_connection.SessionLocal()
+    if not check_db_connection(db):
+        logger.error("Failed to connect to referendum database")
+        raise ConnectionError("referendum database connection failed")
     try:
         yield db
     finally:
@@ -44,93 +50,25 @@ def check_db_connection(db_session):
         raise
 
 
-def extract(etl_configs: List[ETLConfig]) -> List[ETLConfig]:
+def extract_all(etl_configs: List[ETLConfig]):
     legiscan_db = next(get_legiscan_api_db())
 
-    if check_db_connection(legiscan_db):
-        with legiscan_db.connection() as conn:
-            for config in etl_configs:
-                logger.info(f"extracting table {config.source}")
-                try:
-                    df = pd.read_sql(config.source, con=conn)
-
-                    for transformation in config.transformations:
-                        if transformation.function == TransformationFunction.KEEP_COLUMNS:
-                            columns_to_keep = transformation.parameters.columns
-                            df = df[columns_to_keep]
-
-                    config.dataframe = df
-
-                except Exception as e:
-                    logger.error(f"Error processing table {config.source}: {e}")
-
-        return etl_configs
-    else:
-        logger.error("Failed to connect to Legiscan API database. Extraction aborted.")
-        raise ConnectionError("Legiscan API database connection failed")
+    with legiscan_db.connection() as conn:
+        for config in etl_configs:
+            config.extract(conn)
 
 
-def transform(etl_configs: List[ETLConfig]) -> List[ETLConfig]:
+def transform_all(etl_configs: List[ETLConfig]):
     for config in etl_configs:
-        logger.info(f"Transforming table from {config.source} to {config.destination}")
-        try:
-            df = config.dataframe
-
-            for transformation in config.transformations:
-                match transformation.function:
-                    case TransformationFunction.RENAME:
-                        column_mapping = transformation.parameters.columns
-                        df = df.rename(columns=column_mapping)
-
-                    # TODO - generalize to conditional operation
-                    case TransformationFunction.SET_PRIMARY_SPONSOR:
-                        df[transformation.parameters.is_primary_column] = df[transformation.parameters.sponsor_type_column] == 1
-                        df = df.drop(columns=[transformation.parameters.sponsor_type_column])
-
-                    case TransformationFunction.DUPLICATE:
-                        df[transformation.parameters.destination_name] = df[transformation.parameters.source_name]
-
-            config.dataframe = df
-
-        except Exception as e:
-            logger.error(f"Error transforming data for source table {config.source}: {e}")
-
-    return etl_configs
+        config.transform()
 
 
-def load(etl_configs: List[ETLConfig]):
+def load_all(etl_configs: List[ETLConfig]):
     referendum_db = next(get_referendum_db())
 
-    if check_db_connection(referendum_db):
-        try:
-            inspector = sqlalchemy.inspect(referendum_db.connection())
-            tables = inspector.get_table_names()
-
-            logger.info(f"Tables in Referendum database: {tables}")
-            for table in tables:
-                columns = inspector.get_columns(table)
-                column_names = [column["name"] for column in columns]
-                logger.info(f"Columns in '{table}' table: {column_names}")
-        except Exception as e:
-            logger.error(f"Error fetching table metadata: {e}")
-            raise
-
+    with referendum_db.connection() as conn:
         for config in etl_configs:
-            try:
-                logger.info(f"Loading data into {config.destination}")
-                config.dataframe.to_sql(
-                    config.destination,
-                    referendum_db.bind,
-                    if_exists="append",
-                    index=False,
-                )
-
-            except Exception as e:
-                logger.error(f"Error inserting data into '{config.destination}': {e}")
-                raise
-    else:
-        logger.error("Failed to connect to Referendum database. Load aborted.")
-        raise ConnectionError("Referendum database connection failed")
+            config.load(conn)
 
 
 def orchestrate_etl():
@@ -144,11 +82,11 @@ def orchestrate_etl():
     try:
         logger.info("ETL process starting")
         logger.info("Beginning extraction")
-        etl_configs = extract(etl_configs)
+        extract_all(etl_configs)
         logger.info("Beginning transformation")
-        etl_configs = transform(etl_configs)
+        transform_all(etl_configs)
         logger.info("Beginning load")
-        load(etl_configs)
+        load_all(etl_configs)
         logger.info("ETL process completed successfully")
     except ConnectionError as e:
         logger.error(f"ETL process failed: {str(e)}")
