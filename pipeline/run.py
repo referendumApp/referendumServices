51e9de528208c464d7582d83f8b4590b96f0d85e
Henry <75866642+henrydalton@users.noreply.github.com>
2024-10-30 10:27:33 -0700

    everything is broken (#80)
    Co-authored-by: Henry <hrdalton@willamette.edu>
    Co-authored-by: Makani-C <61200430+Makani-C@users.noreply.github.com>

--- pipeline/run.py
+++ pipeline/run.py
@@ -1,6 +1,8 @@
 import logging
 import pandas as pd
 import sqlalchemy
+import json
+import os
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 from typing import Dict
@@ -49,8 +51,7 @@ def extract(etl_configs) -> Dict[str, pd.DataFrame]:
         with legiscan_db.connection() as conn:
             for config in etl_configs:
                 table_name = config["source"]
-                logger.info(f"Extracting table {table_name}")
-
+                logger.info(f"extracting table {table_name}")
                 try:
                     df = pd.read_sql(table_name, con=conn)
 
@@ -77,10 +78,10 @@ def transform(etl_configs) -> Dict[str, pd.DataFrame]:
         logger.info(f"Transforming table {table_name}")
 
         try:
-
             df = config["dataframe"]
 
             for transformation in config.get("transformations", []):
+
                 if transformation["function"] == "rename":
                     columns_to_rename = transformation["parameters"]["columns"]
                     df = df.rename(columns=columns_to_rename)
@@ -96,6 +97,14 @@ def transform(etl_configs) -> Dict[str, pd.DataFrame]:
                     source_name = transformation["parameters"]["source_name"]
                     destination_name = transformation["parameters"]["destination_name"]
                     df[destination_name] = df[source_name]
+
+                elif transformation["function"] == "create_image_url":
+                    name_column = transformation["parameters"]["columns"]
+
+                    # Transform the name for image URL creation
+                    df["image_url"] = df[name_column].apply(
+                        lambda name: f"https://s3.amazonaws.com/ballotpedia-api4/files/thumbs/200/300/{'_'.join([part.capitalize() for part in name.split()])}.jpg"
+                    )
 
             config["dataframe"] = df
 
@@ -135,6 +144,7 @@ def load(etl_configs):
                     if_exists="append",
                     index=False,
                 )
+
             except Exception as e:
                 logger.error(f"Error inserting data into '{destination_table}': {e}")
                 raise
@@ -144,216 +154,12 @@ def load(etl_configs):
 
 
 def orchestrate_etl():
-    etl_configs = [
-        {
-            "source": "ls_state",
-            "destination": "states",
-            "transformations": [
-                {
-                    "function": "keep_columns",
-                    "parameters": {"columns": ["state_id", "state_name"]},
-                },
-                {
-                    "function": "rename",
-                    "parameters": {"columns": {"state_id": "id", "state_name": "name"}},
-                },
-            ],
-            "dataframe": None,
-        },
-        {
-            "source": "ls_role",
-            "destination": "roles",
-            "transformations": [
-                {
-                    "function": "keep_columns",
-                    "parameters": {"columns": ["role_id", "role_name"]},
-                },
-                {
-                    "function": "rename",
-                    "parameters": {"columns": {"role_id": "id", "role_name": "name"}},
-                },
-            ],
-            "dataframe": None,
-        },
-        {
-            "source": "ls_body",
-            "destination": "legislative_bodys",
-            "transformations": [
-                {
-                    "function": "keep_columns",
-                    "parameters": {"columns": ["body_id", "state_id", "role_id"]},
-                },
-                {
-                    "function": "rename",
-                    "parameters": {"columns": {"body_id": "id"}},
-                },
-            ],
-            "dataframe": None,
-        },
-        {
-            "source": "ls_party",
-            "destination": "partys",
-            "transformations": [
-                {
-                    "function": "keep_columns",
-                    "parameters": {"columns": ["party_id", "party_name"]},
-                },
-                {
-                    "function": "rename",
-                    "parameters": {"columns": {"party_id": "id", "party_name": "name"}},
-                },
-            ],
-            "dataframe": None,
-        },
-        {
-            "source": "ls_bill",
-            "destination": "bills",
-            "transformations": [
-                {
-                    "function": "keep_columns",
-                    "parameters": {
-                        "columns": [
-                            "bill_id",
-                            "title",
-                            "description",
-                            "state_id",
-                            "body_id",
-                            "bill_number",
-                            "session_id",
-                            "status_id",
-                            "status_date",
-                        ]
-                    },
-                },
-                {
-                    "function": "rename",
-                    "parameters": {
-                        "columns": {
-                            "bill_id": "id",
-                            "bill_number": "identifier",
-                            "body_id": "legislative_body_id",
-                        }
-                    },
-                },
-                {
-                    "function": "duplicate",
-                    "parameters": {
-                        "source_name": "id",
-                        "destination_name": "legiscan_id",
-                    },
-                },
-            ],
-            "dataframe": None,
-        },
-        {
-            "source": "ls_people",
-            "destination": "legislators",
-            "transformations": [
-                {
-                    "function": "keep_columns",
-                    "parameters": {
-                        "columns": [
-                            "people_id",
-                            "name",
-                            "party_id",
-                            "state_id",
-                            "district",
-                        ]
-                    },
-                },
-                {
-                    "function": "rename",
-                    "parameters": {"columns": {"people_id": "id"}},
-                },
-                {
-                    "function": "duplicate",
-                    "parameters": {
-                        "source_name": "id",
-                        "destination_name": "legiscan_id",
-                    },
-                },
-            ],
-            "dataframe": None,
-        },
-        {
-            "source": "ls_committee",
-            "destination": "committees",
-            "transformations": [
-                {
-                    "function": "keep_columns",
-                    "parameters": {
-                        "columns": [
-                            "committee_id",
-                            "committee_body_id",
-                            "committee_name",
-                        ]
-                    },
-                },
-                {
-                    "function": "rename",
-                    "parameters": {
-                        "columns": {
-                            "committee_id": "id",
-                            "committee_body_id": "legislative_body_id",
-                            "committee_name": "name",
-                        }
-                    },
-                },
-            ],
-            "dataframe": None,
-        },
-        # {
-        #     "source": "ls_bill_vote",
-        #     "destination": "bill_actions",
-        #     "transformations": [
-        #         {
-        #             "function": "keep_columns",
-        #             "parameters": {
-        #                 "columns": [
-        #                     "bill_id",
-        #                     "created",
-        #                     "passed",
-        #                 ]
-        #             },
-        #         },
-        #         {
-        #             "function": "rename",
-        #             "parameters": {
-        #                 "columns": {
-        #                     "created": "date",
-        #                     "passed": "type",
-        #                 }
-        #             },
-        #         },
-        #     ],
-        #     "dataframe": None,
-        # },
-        # {
-        #     "source": "ls_bill_sponsor",
-        #     "destination": "bill_sponsors",
-        #     "transformations": [
-        #         {
-        #             "function": "keep_columns",
-        #             "parameters": {
-        #                 "columns": ["bill_id", "people_id", "sponsor_type_id"]
-        #                 ### bill_id has relationship with bill.id, which we create ??? ###
-        #             },
-        #         },
-        #         {
-        #             "function": "rename",
-        #             "parameters": {"columns": {"people_id": "legislator_id"}},
-        #         },
-        #         {
-        #             "function": "set_primary_sponsor",
-        #             "parameters": {
-        #                 "sponsor_type_column": "sponsor_type_id",
-        #                 "is_primary_column": "is_primary"
-        #             }
-        #         }
-        #     ],
-        #     "dataframe": None,
-        # },
-    ]
+    directory = os.path.dirname(os.path.abspath(__file__))
+    config_filepath = f"{directory}/etl_configs.json"
+
+    with open(config_filepath, "r") as config_file:
+        etl_configs = json.load(config_file)
+
     try:
         logger.info("ETL process starting")
         logger.info("Beginning extraction")
