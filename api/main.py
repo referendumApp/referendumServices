081f4754fbd6ac64052af3ac81ada238535c4b5b
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-11-22 08:56:12 -0800

    Custom API docs location

--- api/main.py
+++ api/main.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 
 s3 = boto3.client("s3")
 
-app = FastAPI()
+app = FastAPI(openapi_url=f"/{settings.ENVIRONMENT}/openapi.json")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
