3e7ff767e55170fc2dcbfedabb8cb8926d8b8bf5
Makani-C <61200430+Makani-C@users.noreply.github.com>
2024-11-22 20:22:41 -0800

    change

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -e
 
-exec uvicorn api.main:app --host 0.0.0.0 --port 80 --log-config None
+exec python -m uvicorn api.main:app \
+    --host 0.0.0.0 \
+    --port 80 \
+    --log-level info \
+    --no-access-log
