87bfa4c84643c4c51c48788d0936e34399a424cd
Ken <k.ng993@gmail.com>
2025-01-12 13:49:48 -0500

    revision

--- alembic/versions/aa2fdd5f0d54_create_text_search_gin_indexes.py
+++ alembic/versions/aa2fdd5f0d54_create_text_search_gin_indexes.py
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "aa2fdd5f0d54"
-down_revision: Union[str, None] = "296fffa291c7"
+down_revision: Union[str, None] = "d45d8a97e784"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
