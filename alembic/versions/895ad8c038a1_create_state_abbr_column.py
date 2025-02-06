17a4dcb827b32983921e05d1caf15907c30ecd6c
Makani-C <61200430+Makani-C@users.noreply.github.com>
2025-02-06 10:40:04 -0800

    fix: Allow abbr to be null (#196)

--- alembic/versions/895ad8c038a1_create_state_abbr_column.py
+++ alembic/versions/895ad8c038a1_create_state_abbr_column.py
@@ -20,7 +20,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column("states", sa.Column("abbr", sa.String(), nullable=False))
+    op.add_column("states", sa.Column("abbr", sa.String(), nullable=True))
 
     op.create_foreign_key(
         "fk_representing_state_id",
