a42c698f96d37a43e993709bb2f5103c56b4a0c3
Ken <k.ng993@gmail.com>
2025-03-03 19:43:23 -0500

    user api

--- atp_service/internal/domain/common/validate.go
+++
@@ -1,9 +0,0 @@
-// Contains interface with HTTP validation methods
-
-package common
-
-import "context"
-
-type Validator interface {
-	Validate(ctx context.Context) (problems map[string]string)
-}
