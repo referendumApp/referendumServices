e44baffb5270816938bf8d86f48258a7f1b1a5b3
Makani-C <61200430+Makani-C@users.noreply.github.com>
2025-05-26 19:07:38 -0700

    and all the rest

--- user_service/internal/app/identity.go
+++ user_service/internal/app/identity.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"net/http"
 	"strings"
 	"time"
 
@@ -112,11 +113,11 @@ func (v *View) GetAuthenticatedActor(
 		v.log.ErrorContext(ctx, "Actor has no auth settings", "username", username)
 		return nil, defaultErr.NotFound()
 	}
-	if actor.AuthSettings.HashedPassword == "" {
+	if *actor.AuthSettings.HashedPassword == "" {
 		v.log.ErrorContext(ctx, "Actor has no hashed password", "username", username)
 		return nil, defaultErr.NotFound()
 	}
-	if ok, verr := util.VerifyPassword(pw, actor.AuthSettings.HashedPassword); verr != nil {
+	if ok, verr := util.VerifyPassword(pw, *actor.AuthSettings.HashedPassword); verr != nil {
 		v.log.ErrorContext(ctx, "Error verifying password", "error", verr, "username", username)
 		return nil, refErr.InternalServer()
 	} else if !ok {
@@ -235,4 +236,52 @@ func (v *View) UpdateUserProfile(
 	}
 
 	return nil
+}
+
+// CreateAdminApiKey creates a new system user with an API key
+func (a *View) CreateAdminApiKey(ctx context.Context) (string, *refErr.APIError) {
+	var apiKey = "LOCAL_TEST_API_KEY" // #nosec G101
+
+	return apiKey, nil
+}
+
+func (a *View) AuthorizeSystemUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestCtx := r.Context()
+
+		a.log.ErrorContext(requestCtx, "authorization header", "header", r.Header)
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			a.log.ErrorContext(requestCtx, "Missing Authorization header")
+			refErr.Unauthorized("Missing Authorization header").WriteResponse(w)
+			return
+		}
+
+		const bearerPrefix = "Bearer "
+		if !strings.HasPrefix(authHeader, bearerPrefix) {
+			a.log.ErrorContext(requestCtx, "Invalid Authorization header format")
+			refErr.Unauthorized("Invalid Authorization header format").WriteResponse(w)
+			return
+		}
+
+		var apiKey = strings.TrimSpace(authHeader[len(bearerPrefix):])
+		if apiKey == "" {
+			a.log.ErrorContext(requestCtx, "Empty API key in Authorization header")
+			refErr.Unauthorized("Empty API key").WriteResponse(w)
+			return
+		}
+
+		aid, did, err := util.ValidateApiKey(apiKey)
+		a.log.ErrorContext(requestCtx, "Got Actor info", "aid", aid, "did", did, "err", err)
+		if err != nil {
+			a.log.ErrorContext(requestCtx, "Failed validate access token", "error", err)
+			refErr.Unauthorized("Invalid token type for access token").WriteResponse(w)
+			return
+		}
+
+		didCtx := context.WithValue(requestCtx, util.DidKey, did)
+		ctx := context.WithValue(didCtx, util.SubjectKey, aid)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
--- user_service/internal/service/routes.go
+++ user_service/internal/service/routes.go
@@ -11,12 +11,14 @@ func (s *Service) setupRoutes() {
 		r.Post("/refresh", s.handleRefreshSession)
 		r.With(s.pds.AuthorizeUser).Delete("/session", s.handleDeleteSession)
 		r.With(s.pds.AuthorizeUser).Delete("/account", s.handleDeleteUser)
+
+		r.With(s.av.AuthorizeSystemUser).Post("/system", s.handleCreateAdmin)
 	})
 
 	s.mux.Route("/user", func(r chi.Router) {
 		r.Use(s.pds.AuthorizeUser)
 
-		r.Put("/profile", s.handleUserProfileUpdate)
+		r.Put("/profile", s.handleUpdateUserProfile)
 		r.Post("/follow", s.handleGraphFollow)
 
 		r.Get("/profile", s.handleGetUserProfile)
@@ -25,13 +27,17 @@ func (s *Service) setupRoutes() {
 	})
 
 	s.mux.Route("/legislator", func(r chi.Router) {
-		// TODO - add system auth here
-		// 		r.Use(s.pds.AuthorizeUser)
+		r.Group(func(r chi.Router) {
+			r.Use(s.av.AuthorizeSystemUser)
+			r.Post("/", s.handleCreateLegislator)
+			r.Put("/", s.handleUpdateLegislator)
+			r.Delete("/", s.handleDeleteLegislator)
+		})
 
-		r.Post("/", s.handleCreateLegislator)
-		r.Get("/", s.handleGetLegislator)
-		r.Put("/", s.handleUpdateLegislator)
-		r.Delete("/", s.handleDeleteLegislator)
+		r.Group(func(r chi.Router) {
+			// r.Use(s.pds.AuthorizeAdminOrUser)
+			r.Get("/", s.handleGetLegislator)
+		})
 	})
 
 	s.mux.Route("/server", func(r chi.Router) {
